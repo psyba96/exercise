4868c65f846029088c151f1aae91a4086baae20a
junaid <shaikh.junaid4@gmail.com>
2025-08-30 19:11:36 +1000

    2.0.0 #deleted irrelevant files

--- src/main/java/org/js/ExerciseRepository.java
+++
@@ -1,7 +0,0 @@
-package org.js;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-import org.springframework.data.repository.ListCrudRepository;
-
-public interface ExerciseRepository extends ListCrudRepository<Exercise, Integer>, JpaSpecificationExecutor<Exercise> {
-    boolean existsByName(String name);
-}
--- src/main/java/org/js/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package org.js;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
